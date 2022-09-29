e86e4b4ad98e96d8f07c9c57ce22760cf8b58046
Marcus Henglein <marcus@henglein.com>
2022-09-29 20:59:46 +0200

    x

---
+++ controllers/openai.js
@@ -0,0 +1,18 @@
+"use strict";
+require("dotenv").config({ path: "config/.env" });
+const { Configuration, OpenAIApi } = require("openai");
+const { encode } = require("gpt-3-encoder");
+
+// OpenAI configuration
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports.summarizer = async (text) => {
+  // Get length of text: Regular summary or recursive required?
+  // Use encode from gpt3
+  // Break text into chunks of X characters
+  // Step 1 Initial summary (Prompt 1)
+  // Step 2 Recursive summary (Prompt 2)
+};
